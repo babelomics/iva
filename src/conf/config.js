f0669c9724f7564bf327730903e96aa2ad8c1513
jlfernandez-cba <josel.fernandez.rueda@juntadeandalucia.es>
2018-10-29 12:20:48 +0100

    several improvements to interpretation screen

--- src/conf/config.js
+++ src/conf/config.js
@@ -778,3 +778,55 @@ const DEFAULT_SPECIES = {
         },
     ],
 };
+
+
+
+// scoring for consequence types
+
+const consequenceTypesComparator = (type1, type2) => {
+
+    // http://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
+    const consequenceTypes = [
+        'transcript_ablation',
+        'splice_acceptor_variant',
+        'splice_donor_variant',
+        'stop_gained',
+        'frameshift_variant',
+        'stop_lost',
+        'start_lost',
+        'transcript_amplification',
+        'inframe_insertion',
+        'inframe_deletion',
+        'missense_variant',
+        'protein_altering_variant',
+        'splice_region_variant',
+        'incomplete_terminal_codon_variant',
+        'start_retained_variant',
+        'stop_retained_variant',
+        'synonymous_variant',
+        'coding_sequence_variant',
+        'mature_miRNA_variant',
+        '5_prime_UTR_variant',
+        '3_prime_UTR_variant',
+        'non_coding_transcript_exon_variant',
+        'intron_variant',
+        'NMD_transcript_variant',
+        'non_coding_transcript_variant',
+        'upstream_gene_variant',
+        'downstream_gene_variant',
+        'TFBS_ablation',
+        'TFBS_amplification',
+        'TF_binding_site_variant',
+        'regulatory_region_ablation',
+        'regulatory_region_amplification',
+        'feature_elongation',
+        'regulatory_region_variant',
+        'feature_truncation',
+        'intergenic_variant',
+    ];
+    let idx1 = consequenceTypes.indexOf(type1);
+    idx1 = idx1 >= 0 ? idx1 : consequenceTypes.length;
+    let idx2 = consequenceTypes.indexOf(type2);
+    idx2 = idx2 >= 0 ? idx2 : consequenceTypes.length;
+    return idx1 - idx2;
+}
