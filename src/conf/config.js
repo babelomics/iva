ba2044061e245956f36165288ec3546c0dab951b
jlfernandez-cba <josel.fernandez.rueda@juntadeandalucia.es>
2018-11-19 11:52:40 +0100

    fixes several bugs, such as opencga-variant-interpretation.connectedCallback using this in a callback, incorrect state persistence in interpretation screen, the inability to manage variants in haploid genes, and several others

--- src/conf/config.js
+++ src/conf/config.js
@@ -113,7 +113,7 @@ const opencga = {
 
 const application = {
     title: "MMP-SAS",
-    version: "v1.1.10",
+    version: "v1.1.12",
     logo: "img/cbra_logo_initial.png",
     notifyEventMessage: "notifymessage",
     mmpExtensionWS: {
